4e9494e45582488f7974f6c4851a8ef814f1c290
niraj <niraj@mapmyindia.com>
2024-10-14 18:09:37 +0530

    github workflow updated

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: 'dist', // Ensure the output directory is correct
+    assetsDir: 'assets', // Default is 'assets', ensure it is not changed
+    sourcemap: true, // Optional, for debugging purposes
+  }
 });
